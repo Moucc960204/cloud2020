df2630a7925b55c6086b6acde886b9dcc153f821
Moucc960204 <835215156@qq.com>
2020-07-20 22:34:57 +0800

    ribbon

---
+++ cloud2020/cloud-consumer-order80/src/main/java/com/study/myrule/MySelfrule.java
@@ -0,0 +1,21 @@
+package com.study.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: chenchen.mou
+ * @Description:
+ * @Date: create in 2020/7/20 21:57
+ */
+@Configuration
+public class MySelfrule {
+
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();// 定义为随机
+    }
+
+}
--- cloud2020/cloud-consumer-order80/src/main/java/com/study/springcloud/OrderMain80.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/study/springcloud/OrderMain80.java
@@ -9,9 +9,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @Description:
  * @Date: create in 2020/7/5 14:59
  */
-
 @SpringBootApplication
 @EnableEurekaClient
+//@RibbonClient(name = "CLOUD-PROVIDER-SERVICE", configuration = MySelfrule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
